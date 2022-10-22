72166a2a82392e17001a89e2d25d0d2cb81924ba
matthewmak0315 <81973522+matthewmak0315@users.noreply.github.com>
2022-10-22 16:13:47 +0800

    TEST

--- src/main/java/Lab1/Library.java
+++ src/main/java/Lab1/Library.java
@@ -8,7 +8,7 @@ public class Library {
 	public static void main(String arg[]) {
 		Library lib = new Library();
 		if(lib.someLibraryMethod())
-		System.out.println("Hello, World!!!");
+		System.out.println("Hello, World!!!!");
 		
 	}
 	
